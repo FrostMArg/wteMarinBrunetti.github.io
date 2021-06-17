e52f3ead1b0b1e6a8bf35c68bee1c1046891e251
Matias Marin <matias.slipknot@gmail.com>
2021-06-16 21:27:56 -0300

    Dejando una nota para avisar donde arrancan las soluciones de los desafios

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
 import { NavBar } from './components/Navbar/NavBar.js';
-//import { ItemListContainer } from './screens/ItemListContainer/ItemListContainer';
+
+
+import { ItemListContainer } from './screens/ItemListContainer/ItemListContainer';
 
 import { ItemDetailContainer } from './screens/ItemDetailContainer/ItemDetailContainer.js'
+
+
+
 function App() {
   return <>
     <NavBar />
-    {/* <ItemListContainer /> */}
+    <h2> El desafio de la clase 6 comienza aqui debajo</h2>
+    <ItemListContainer />
+    <h2> El desafio de la clase 7 comienza aqui debajo</h2>
     <ItemDetailContainer />
   </>
 }
