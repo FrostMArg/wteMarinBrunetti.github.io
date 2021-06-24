fffcde4273f72dd72e3b998db68c07448ce25605
Matias Marin <matias.slipknot@gmail.com>
2021-06-24 10:36:54 -0300

    Chau switch, hola if - ItemListContainer

--- src/screens/ItemListContainer/ItemListContainer.js
+++ src/screens/ItemListContainer/ItemListContainer.js
@@ -3,11 +3,7 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import Grid from '@material-ui/core/Grid';
 import { ItemList } from './ItemList/ItemList.js';
-import { itemListStyles } from './ItemList/ItemListStyles.js';
-import { makeStyles } from '@material-ui/core/styles';
-const useStyle = makeStyles((theme) => itemListStyles(theme));
 export const ItemListContainer = ({ productosObjeto }) => {
-    const styles = useStyle();
     const [promStatus, setPromStatus] = useState('Pending');
     const [productos, setProductos] = useState([]);
     const tipoPeriferico = useParams();
@@ -26,24 +22,22 @@ export const ItemListContainer = ({ productosObjeto }) => {
         promesaProductos();
         const filterByCategory = (res) => {
             let productosFiltrados;
-            switch (tipoPeriferico.id) {
-                case 'Headset': productosFiltrados = res.filter((productos) => productos.categoria === 'Headset');
-                    break;
-                case 'Mouse': productosFiltrados = res.filter((productos) => productos.categoria === 'Mouse');
-                    break;
-                default: productosFiltrados = res;
-                    break;
+            if (tipoPeriferico.id) {
+                productosFiltrados = res.filter(
+                    (productos) => productos.categoria === tipoPeriferico.id
+                );
+            } else {
+                // Si no encuentro el id de la categoria, muestro todo lo que trae RES
+                productosFiltrados = res;
             }
             setProductos(productosFiltrados);
         };
     }, [tipoPeriferico, productosObjeto]);
-    return (<>
-        <Grid container>
-            <Grid item xs={12}>
-                <Grid container justify="center" >
-                    <ItemList status={promStatus} productos={productos} />
-                </Grid>
+    return <Grid container>
+        <Grid item xs={12}>
+            <Grid container justify="center" >
+                <ItemList status={promStatus} productos={productos} />
             </Grid>
         </Grid>
-    </>);
+    </Grid>
 };
