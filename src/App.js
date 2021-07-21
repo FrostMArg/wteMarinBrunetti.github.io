7fe11b6ce65822a854becd8bbc4cfcfb93bd2314
Matias Marin <matias.slipknot@gmail.com>
2021-07-20 21:02:17 -0300

    update case sensitive in folders

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Router } from './Router/Router.js';
 import { CartProvider } from './Context/Context.js';
 
-import Footer from './Components/Footer/Footer.js'
+import Footer from './Components/Footer/Footer.js';
 function App() {
   return <BrowserRouter>
     <CartProvider>
